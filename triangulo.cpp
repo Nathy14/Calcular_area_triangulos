5ddf4a4a066ba9cd2e8ac7c770002b1acf217187
Natália <natalia.gama.mattos@hotmail.com>
2019-03-20 10:29:44 -0300

    Update triangulo.cpp

--- triangulo.cpp
+++ triangulo.cpp
@@ -1,30 +1,27 @@
 #include "triangulo.h"
 #include <math.h>
 
-Triangulo::Triangulo(int a, int b, int c){
+triangulo::triangulo(double a, double b, double c){
   Lado1 = a;
   Lado2 = b;
   Lado3 = c;
 }
-Triangulo::~Triangulo(){}
-double Triangulo::area(){
+triangulo::~triangulo(){}
+double triangulo::area(){
   double p = (Lado1+Lado2+Lado3)/2;
   double areazinha = sqrt(p*(p-Lado1)*(p-Lado2)*(p-Lado3));
   return areazinha;
 }
-double Triangulo::GetLado1(){
+double triangulo::GetLado1(){
   return Lado1;
 }
-double Triangulo::GetLado2(){
+double triangulo::GetLado2(){
   return Lado2;
 }
-double Triangulo::GetLado3(){
+double triangulo::GetLado3(){
   return Lado3;
 }
-Triangulo::Triangulo operator+(Triangulo t){
-  double a = t.GetLado1();
-  double b = t.GetLado2();
-  double c = t.GetLado3();
-  double soma = t.area() + area();
+triangulo triangulo::operator+(triangulo t){
+  double soma = area()+t.area();
   return soma;
 }
